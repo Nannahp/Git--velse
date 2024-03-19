92f93718275bd6d0c2c9dafee3021817817e4010
BlablaTara <tara0561@stud.kea.dk>
2024-03-19 12:20:28 +0100

    Herjgh

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,8 @@ public class Main {
         System.out.println("ps sej hat");
 
         System.out.println("Java kode på høj plan XP ");
+        System.out.println("Java kode på høj plan XP ");
+        System.out.println("Java kode på høj plan XP ");
 
     }
 
