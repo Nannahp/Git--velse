c4b52ed522cab160ee38cf201fd7b952e6c669f4
Nanna Petersen <nannahofgaard@hotmail.com>
2024-03-19 11:51:27 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Halllooo");
+        System.out.println("Halll");
         System.out.println("jamen hej");
 
         System.out.println("From the other siiiiiiide!");
